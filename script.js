1a9cbd50cb0685d6f9e9a405c9e06bf51c8b30ca
priyanka-bera <144355970+priyanka-bera@users.noreply.github.com>
2023-09-08 20:58:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new typed(".text", {
+        strings: ["Web Developer", "Html Developer", "Frontend Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
